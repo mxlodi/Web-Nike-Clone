dc820ff49d31ae6c266c76005aa13760a884d4fc
lafang <raksasparkle@gmail.com>
2024-05-23 21:30:37 +0700

    file : add featured.js component

--- src/script/components/featured.js
+++ src/script/components/featured.js
@@ -1,12 +1,13 @@
-const featured = (data) => {
+const featured = (data,title) => {
     return `
     <div class="max-w-[1850px] mx-auto p-3 mt-10"> 
+    <h2 class="text-2xl mb-5">${title}</h2> 
+    <div class="flex gap-3 md:flex-row cursor-pointer flex-col"> 
+
         ${data.map((card) => {
             return`
-            <h2 class="text-2xl mb-5">Featured</h2> 
-            <div class="flex gap-3 md:flex-row cursor-pointer flex-col"> 
-                <!-- CARD 1 --> 
-                <div class="w-full h-[680px] relative "> 
+                <!-- CARD --> 
+                <div class="w-full relative "> 
                     <img class="w-full h-full object-cover block md:hidden" src="${card.imgURL_1}" alt=""> 
                     <img class="w-full h-full object-cover md:block hidden"  src="${card.imgURL_2}" alt="">
                     <div class="absolute bottom-8 left-8 p-2 text-white"> 
@@ -15,18 +16,20 @@ const featured = (data) => {
                         <a href="../../pages/newfeatured/newfeatured.html" class="bg-white px-4 py-1.5 rounded-full font-semibold text-black mt-4 hover:bg-gray-200">${card.button}</a> 
                     </div> 
                 </div> 
-            </div> 
+
             `;
         }).join("")}
+    </div> 
     </div>   
     `;
 };
-
-class Featured extends HTMLElement {
-    connectedCallback(){
-        const listItems = JSON.parse(this.getAttribute("listItems"));
-        this.innerHTML = featured(listItems);
+export function createFeaturedContent(arr,title, componentName){
+    class Featured extends HTMLElement {
+        connectedCallback(){
+            this.innerHTML = featured(arr, title);
+     
+        }
     }
+    customElements.define(componentName, Featured);
 }
 
-customElements.define("featured-component", Featured);
