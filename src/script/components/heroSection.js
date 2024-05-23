1ed39f6d873bf499545771c4b5438a57dff41b7e
lafang <raksasparkle@gmail.com>
2024-05-23 21:27:19 +0700

    file : add heroSection.js

--- src/script/components/heroSection.js
+++ src/script/components/heroSection.js
@@ -1,4 +1,4 @@
-const heroSection = (imgURL_1, imgURL_2, name, title, description, button) => {
+const heroSection = (imgURL_1, imgURL_2, nameA, title, description, button) => {
   return `
     <!-- big image -->
     <div class="max-w-[1850px] mx-auto p-3 mt-4">
@@ -13,7 +13,7 @@ const heroSection = (imgURL_1, imgURL_2, name, title, description, button) => {
 
     <div class="p-4">
         <div class="mt-10 cursor-pointer mx-auto text-start sm:text-center">
-            <h1 class="lg:text-lg text-base">${name}</h1>
+            <h1 class="lg:text-lg text-base">${nameA}</h1>
             <h2 class="text-7xl passion-bold tracking-tight ">${title}</h2>
             <h2 class="my-6">${description}</h2>
             <a href="../../pages/newfeatured/newfeatured.html" class="bg-black text-white py-1 px-3 rounded-full hover:opacity-70">${button}</a>
@@ -22,16 +22,24 @@ const heroSection = (imgURL_1, imgURL_2, name, title, description, button) => {
 
     `;
 };
+export function createHeroSection(arr, componentName){
 
 class HeroSection extends HTMLElement {
+    constructor(){
+        super();
+    }
     connectedCallback(){
-        const imgURL_1 = this.getAttribute("imgURL_1"); 
-        const imgURL_2 = this.getAttribute("imgURL_2");
-        const name = this.getAttribute("name");
-        const title = this.getAttribute("title");
-        const description = this.getAttribute("description");
-        const button = this.getAttribute("customButton");
-        this.innerHTML = heroSection(imgURL_1, imgURL_2, name, title, description, button);
+        arr.map((item)=>{
+            this.innerHTML = heroSection(item.imgURL_1, item.imgURL_2, item.nameA, item.title, item.description, item.button);
+        })
+        // const imgURL_1 = this.getAttribute("imgURL_1"); 
+        // const imgURL_2 = this.getAttribute("imgURL_2");
+        // const name = this.getAttribute("name");
+        // const title = this.getAttribute("title");
+        // const description = this.getAttribute("description");
+        // const button = this.getAttribute("customButton");
+
     }
 }
-customElements.define("hero-section-component", HeroSection);
+customElements.define(componentName, HeroSection);
+}
