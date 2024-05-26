46db5b8901f9f4ae23aa0ce2ee572cbcd3f43810
lafang <raksasparkle@gmail.com>
2024-05-26 21:20:26 +0700

    file : add header.js to script

--- src/script/components/header.js
+++ src/script/components/header.js
@@ -1,4 +1,4 @@
-const header = (marquee, data, title) => {
+const header = (marquee, data) => {
     return `
     <div class="bg-[#f5f5f5] h-[60px] flex justify-center gap-8 items-center">
         <button><img class="w-5" src="../../images/home/trending/left-chevron.png" alt=""></button>
@@ -8,27 +8,28 @@ const header = (marquee, data, title) => {
 
     <div id="menMenu" class="w-full bg-white z-20 sticky top-0">
         <div class="max-w-[1850px] flex md:flex-row flex-col mx-auto p-3 mt-3 overflow-hidden">
-            <h2 id="letterMen" class="text-2xl duration-500">${title}</h2>
-            <ul class="w-full flex gap-8 sm:justify-center justify-start items-center py-2 lg:mx-auto max-w-[700px] font-semibold">
-                <li class="hover:text-black/70 cursor-pointer text-nowrap">${data.small_menu_1}</li>
-                <li class="hover:text-black/70 cursor-pointer text-nowrap">${data.small_menu_2}</li>
-                <li class="hover:text-black/70 cursor-pointer text-nowrap">${data.small_menu_3}</li>
-                <li class="hover:text-black/70 cursor-pointer text-nowrap">${data.small_menu_4}</li>
-                <li class="hover:text-black/70 cursor-pointer text-nowrap">${data.small_menu_5}</li>
-                <li class="hover:text-black/70 cursor-pointer text-nowrap">${data.small_menu_6}</li>
-            </ul>
+            ${data.map((item) => `
+                <div>
+                    <h2 id="letterMen" class="text-2xl duration-500">${item.title}</h2>
+                    <ul class="w-full flex gap-8 sm:justify-center justify-start items-center py-2 lg:mx-auto font-semibold">
+                        ${[item.text_1, item.text_2, item.text_3, item.text_4, item.text_5, item.text_6].map(text => text ? `<li class="hover:text-black/70 cursor-pointer text-nowrap">${text}</li>` : '').join('')}
+                    </ul>
+                </div>
+            `).join('')}
         </div>
     </div>
     `;
 }
 
-class Header extends HTMLElement {
-    constructor(){
-        super();
+export function createHeader(data, marquee, componentName) {
+    class Header extends HTMLElement {
+        constructor() {
+            super();
+        }
+        connectedCallback() {
+            this.innerHTML = header(marquee, data);
+        }
     }
-    connectedCallback() {
-        this.innerHTML = header(marquee, data, title);
-    } 
-}
 
-customElements.define("header-component", Header);
+    customElements.define(componentName, Header);
+}
