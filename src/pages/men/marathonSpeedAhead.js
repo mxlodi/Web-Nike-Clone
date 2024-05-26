b6d563869bdb9522b9641055bfdc6e63379200c4
lafang <raksasparkle@gmail.com>
2024-05-26 21:39:25 +0700

    file: add marathon speed ahead.js to men in pages

---
+++ src/pages/men/marathonSpeedAhead.js
@@ -0,0 +1,26 @@
+const marathonSpeed = () => {
+    return `
+    <div> 
+        <div class="max-w-[1850px] mx-auto p-3 ">
+            <h1 class="translate-y-16 text-2xl">Marathon Speed Ahead</h1>
+        </div>
+
+        <div class="max-w-[2557px] max-h-[951px] mx-auto mt-16 p-3"> 
+            <img src="../../images/men/marathon/marathon-speed-ahead.jpeg" alt="" class="w-full h-full min-[600px]:block hidden object-cover"> 
+            <img src="../../images/men/marathon/men-s-shoes-clothing-accessories (5).jpg" alt="" class="w-full min-[600px]:hidden block">
+        </div> 
+    </div>
+  
+      `;  
+  };
+  // 
+  class MarathonSpeed extends HTMLElement {
+      constructor(){
+          super()
+      }
+      connectedCallback(){
+          
+          this.innerHTML = marathonSpeed();
+      }
+  }
+  customElements.define("marathon-speed-ahead-content", MarathonSpeed);
