9f50dc00427bb69af577bae7b0863bf41b4522af
lafang <raksasparkle@gmail.com>
2024-05-26 21:20:52 +0700

    file : add heroSection.js to script

--- src/script/components/heroSection.js
+++ src/script/components/heroSection.js
@@ -16,7 +16,7 @@ const heroSection = (imgURL_1, imgURL_2, nameA, title, description, button) => {
             <h1 class="lg:text-lg text-base">${nameA}</h1>
             <h2 class="text-7xl passion-bold tracking-tight ">${title}</h2>
             <h2 class="my-6">${description}</h2>
-            <a href="../../pages/newfeatured/newfeatured.html" class="bg-black text-white py-1 px-3 rounded-full hover:opacity-70">${button}</a>
+            <a href="../../pages/newfeatured/newfeatured.html" class="bg-black text-white py-2 px-3 rounded-full hover:opacity-70">${button}</a>
         </div>
     </div>
 
@@ -24,22 +24,15 @@ const heroSection = (imgURL_1, imgURL_2, nameA, title, description, button) => {
 };
 export function createHeroSection(arr, componentName){
 
-class HeroSection extends HTMLElement {
-    constructor(){
-        super();
+    class HeroSection extends HTMLElement {
+        constructor(){
+            super();
+        }
+        connectedCallback(){
+            arr.map((item)=>{
+                this.innerHTML = heroSection(item.imgURL_1, item.imgURL_2, item.nameA, item.title, item.description, item.button);
+            })
+        }
     }
-    connectedCallback(){
-        arr.map((item)=>{
-            this.innerHTML = heroSection(item.imgURL_1, item.imgURL_2, item.nameA, item.title, item.description, item.button);
-        })
-        // const imgURL_1 = this.getAttribute("imgURL_1"); 
-        // const imgURL_2 = this.getAttribute("imgURL_2");
-        // const name = this.getAttribute("name");
-        // const title = this.getAttribute("title");
-        // const description = this.getAttribute("description");
-        // const button = this.getAttribute("customButton");
-
-    }
-}
-customElements.define(componentName, HeroSection);
+    customElements.define(componentName, HeroSection);
 }
