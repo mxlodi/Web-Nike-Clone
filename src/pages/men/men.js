19edb225b283a7fc2d86aba78c7568b6edaa2efb
lafang <raksasparkle@gmail.com>
2024-05-27 21:39:19 +0700

    file : add men folder to components

--- src/pages/men/men.js
+++ src/pages/men/men.js
@@ -5,15 +5,16 @@ import { createHeader } from "../../script/components/header.js";
 import { createCarouselEssential } from "../../script/components/shoptheessential.js";
 import { createFeaturedContent } from "../../script/components/featured.js";
 import { createPopular } from "../../script/components/popular.js";
+import { createClassicSpotlight } from "../../script/components/classicsSpotlight.js";
 import "../../script/components/membership.js"
 import "./heroSectionMen.js";
 import "./marathonSpeedAhead.js"
-import { createClassicSpotlight } from "../../script/components/classicsSpotlight.js";
 
-// header text section
+import "./subFooterMen.js"
+
+// header text section and marquee
 const headerArr = [
     {
-        marquee: "Members: Free Shipping on Orders $50+",
         title: "Men",
         text_1: "Shop",
         text_2: "Clothing",
@@ -38,7 +39,7 @@ const marathonArr = [
 
 
 // shop the essebtial section
-const shoptheessential = [
+const shopTheEssential = [
     {
         imgURL : "../../images/men/shop-the-essentials/men-s-shoes-clothing-accessories.jpg",
         title : "Clothing",
@@ -81,7 +82,7 @@ const brooks = [
         imgURL_1 : "../../images/men/brooks/men-s-shoes-clothing-accessories (3).jpg",
         imgURL_2 : "../../images/men/brooks/men-s-shoes-clothing-accessories (6).jpg",
         title_1 : "Brooks's Golf Essentials",
-        title_2 : "rooks's Golf Essentials",
+        title_2 : "Brooks's Golf Essentials",
         button : "Shop"
     },
     {
@@ -100,51 +101,51 @@ const popularMenArr = [
         title : "Nike Air Max Dn",
         description: "Shoes",
         price : "$160"
-      },
-      {
+    },
+    {
         imgURL : "../../images/men/popular/book-1-hike-basketball-shoes-hQGJ09.png",
         title : `Book 1 "Hike"`,
         description : "Basketball Shoes",
         price : "$150"
-      },
-    
-      {
+    },
+
+    {
         imgURL : "../../images/men/popular/dunk-low-retro-mens-shoes-5FQWGR.png",
         title : "Nike Dunk Low Retro",
         description : "Men's Shoes",
         price : "$115"
-      },
-      {
+    },
+    {
         imgURL : "../../images/men/popular/gt-cut-3-basketball-shoes-GLHCGc (2).png",
         title : "Nike G.T. Cut 3",
         description : "Basketball Shoes",
         price : "$190"
-      },
-      {
+    },
+    {
         imgURL : "../../images/men/popular/zoom-vomero-5-mens-shoes-MgsTqZ.png",
         title : "Nike Zoom Vomero 5",
         description : "Men's Shoes",
         price : "$160"
-      },
-      {
+    },
+    {
         imgURL : "../../images/men/popular/air-max-1-sc-mens-shoes-bNNbQl.png",
         title : "Nike Air Max 1 SC",
         description : "Men's Shoes",
         price : "$112.97"
-      },
-      {
+    },
+    {
         imgURL : "../../images/men/popular/air-force-1-07-mens-shoes-jBrhbr.png",
         title : "Nike Air Force 1 '07",
         description : "Men's Shoes",
         price : "$115"
-      }
+    }
 ];
 
 
 
 createHeroSection(marathonArr, "marathon-text-content");
-createHeader(headerArr, headerArr[0].marquee, "header-content");
-createCarouselEssential(shoptheessential,"Shop the essential", "shoptheessential-content");
+createHeader(headerArr, "Members: Free Shipping on Orders $50+", "header-men-content");
+createCarouselEssential(shopTheEssential,"Shop the Essentials", "shoptheessential-men-content");
 createClassicSpotlight(classicsSpotlightMenArr, "classics-spotlight-men-content");
 createFeaturedContent(brooks,"Brooks Koepka's Favs", "brooks-koepka-content");
 createPopular(popularMenArr, "popular-men-content");
